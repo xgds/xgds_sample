73bc7fc19c67d2eca2b0cf3dc7ccb371f42da5ca
Grace Lee <yeongrace.lee@gmail.com>
2016-02-02 14:11:34 -0800

    create app from template

---
+++ setup.py
@@ -0,0 +1,48 @@
+# __BEGIN_LICENSE__
+# Copyright (c) 2015, United States Government, as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# All rights reserved.
+#
+# The xGDS platform is licensed under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+# __END_LICENSE__
+
+import os
+from setuptools import setup, find_packages
+
+
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+# Use the docstring of the __init__ file to be the description
+DESC = " ".join(__import__('xgds_sample').__doc__.splitlines()).strip()
+
+setup(
+    name="xgds_sample",
+    version=__import__('xgds_sample').get_version().replace(' ', '-'),
+    url='',
+    author='ylee8',
+    author_email='',
+    description=DESC,
+    long_description=read_file('README'),
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=read_file('requirements.txt'),
+    classifiers=[
+        'License :: OSI Approved :: NASA Open Source Agreement',
+        'Framework :: Django',
+    ],
+)
