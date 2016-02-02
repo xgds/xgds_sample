10127e6a2499d99f0ff65a973daeee05fbb354bc
Grace Lee <yeongrace.lee@gmail.com>
2016-02-02 23:04:06 +0000

    trying to get sample up and runnign

--- xgds_sample/forms.py
+++ xgds_sample/forms.py
@@ -1,4 +1,4 @@
-# __BEGIN_LICENSE__
+#__BEGIN_LICENSE__
 # Copyright (c) 2015, United States Government, as represented by the
 # Administrator of the National Aeronautics and Space Administration.
 # All rights reserved.
@@ -12,6 +12,13 @@
 # under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
-# __END_LICENSE__
+#__END_LICENSE__
 
+from django.conf import settings
 from django import forms
+from django.forms import ModelForm
+
+class SampleForm(ModelForm):
+    # TODO: hide creation/modification time and creator/modifier
+    class Meta: 
+        model = settings.XGDS_SAMPLE_SAMPLE_MODEL
