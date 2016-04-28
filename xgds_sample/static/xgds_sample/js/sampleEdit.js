074d170da287f1e3ffe02a609601eba0ed9b42c0
Tamar Cohen <tamar.cohen@nasa.gov>
2016-04-28 16:14:25 -0700

    refactoring basalt stuff out of sample

--- xgds_sample/static/xgds_sample/js/sampleEdit.js
+++ xgds_sample/static/xgds_sample/js/sampleEdit.js
@@ -14,96 +14,63 @@
 // specific language governing permissions and limitations under the License.
 //__END_LICENSE__
 
-
-/*
- * Triplicates Legend:
-	Biology = A, B, C
-	Chemistry = D, E
-	Geology 
-	Archive
- */
-
-function showReplicateOptions() {
-	var selected = $("#id_sample_type option:selected").html();
-	$("#id_replicate").children('option').hide();
-	$("#id_replicate").removeAttr('disabled');
-	if (selected == "Biology") {
-		$("#id_replicate option[value='1']").show();
-		$("#id_replicate option[value='2']").show();
-		$("#id_replicate option[value='3']").show();
-	} else if (selected == "Chemistry") {
-		$("#id_replicate option[value='4']").show();
-		$("#id_replicate option[value='5']").show();
-	} else if ((selected == "Archive") || (selected == "Geology")) {
-		$("#id_replicate").val('');
-		$("#id_replicate").attr('disabled','disabled')
-	} else if (selected = "Special") {
-		$("#id_replicate option[value='6']").show();
-		$("#id_replicate option[value='7']").show();
-		$("#id_replicate option[value='8']").show();
-		$("#id_replicate option[value='9']").show();
-		$("#id_replicate option[value='10']").show();
-	}
-}
-
-$("#id_sample_type").change(function() {
-	showReplicateOptions();
-});
-
-
-function initializeSampleEditForm(){
-	$('#id_resource').parent().parent().hide();
-	$('#id_latitude').parent().parent().hide();
-	$('#id_longitude').parent().parent().hide();
-	$('#id_altitude').parent().parent().hide();
-	$('#id_flight').parent().parent().hide();
-}
-
-function toggleAdvancedInput() {
-	$('#id_resource').parent().parent().toggle();
-	$('#id_latitude').parent().parent().toggle();
-	$('#id_longitude').parent().parent().toggle();
-	$('#id_altitude').parent().parent().toggle();
-	$('#id_flight').parent().parent().toggle();
-	if ($('#id_resource').is(":visible")) {
-		$('.toggleInputFields').html('Close out-of-sim fields');	
-	} else {
-		$('.toggleInputFields').html('Open out-of-sim fields');	
-	}
-}
-
-// typeahead autocomplete for input fields
-
-var substringMatcher = function(strs) {
-	  return function findMatches(q, cb) {
-	    var matches, substringRegex;
-
-	    // an array that will be populated with substring matches
-	    matches = [];
-
-	    // regex used to determine if a string contains the substring `q`
-	    substrRegex = new RegExp(q, 'i');
-
-	    // iterate through the pool of strings and for any string that
-	    // contains the substring `q`, add it to the `matches` array
-	    $.each(strs, function(i, str) {
-	      if (substrRegex.test(str)) {
-	        matches.push(str);
-	      }
-	    });
-
-	    cb(matches);
-	  };
-	};
-
-	
-$('#id_collector').typeahead({
-	  hint: true,
-	  highlight: true,
-	  minLength: 1
+var xgds_sample = xgds_sample || {};
+$.extend(xgds_sample,{
+	initializeSampleEditForm: function(){
+		$('#id_resource').parent().parent().hide();
+		$('#id_latitude').parent().parent().hide();
+		$('#id_longitude').parent().parent().hide();
+		$('#id_altitude').parent().parent().hide();
+		$('#id_flight').parent().parent().hide();
+		this.setupCollectorInput();
 	},
-	{
-	  name: 'collector',
-	  source: substringMatcher(collectors)
-	});
+	toggleAdvancedInput: function() {
+		$('#id_resource').parent().parent().toggle();
+		$('#id_latitude').parent().parent().toggle();
+		$('#id_longitude').parent().parent().toggle();
+		$('#id_altitude').parent().parent().toggle();
+		$('#id_flight').parent().parent().toggle();
+		if ($('#id_resource').is(":visible")) {
+			$('.toggleInputFields').html('Close out-of-sim fields');	
+		} else {
+			$('.toggleInputFields').html('Open out-of-sim fields');	
+		}
+	},
+	setupCollectorInput: function() {
+		// typeahead autocomplete for input fields
 
+		var substringMatcher = function(strs) {
+			  return function findMatches(q, cb) {
+			    var matches, substringRegex;
+
+			    // an array that will be populated with substring matches
+			    matches = [];
+
+			    // regex used to determine if a string contains the substring `q`
+			    substrRegex = new RegExp(q, 'i');
+
+			    // iterate through the pool of strings and for any string that
+			    // contains the substring `q`, add it to the `matches` array
+			    $.each(strs, function(i, str) {
+			      if (substrRegex.test(str)) {
+			        matches.push(str);
+			      }
+			    });
+
+			    cb(matches);
+			  };
+			};
+
+			
+		$('#id_collector').typeahead({
+			  hint: true,
+			  highlight: true,
+			  minLength: 1
+			},
+			{
+			  name: 'collector',
+			  source: substringMatcher(collectors)
+			});
+	}
+
+});
