c5086a993d9b25f7890817e53e477f2aae80cee9
Grace Lee <yeongrace.lee@gmail.com>
2016-05-03 18:38:41 +0000

    merging changes for sampleEdit.js

--- xgds_sample/static/xgds_sample/js/sampleEdit.js
+++ xgds_sample/static/xgds_sample/js/sampleEdit.js
@@ -1,29 +1,65 @@
 //__BEGIN_LICENSE__
-// Copyright (c) 2015, United States Government, as represented by the
-// Administrator of the National Aeronautics and Space Administration.
-// All rights reserved.
-//
-// The xGDS platform is licensed under the Apache License, Version 2.0
-// (the "License"); you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// http://www.apache.org/licenses/LICENSE-2.0.
-//
-// Unless required by applicable law or agreed to in writing, software distributed
-// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-// CONDITIONS OF ANY KIND, either express or implied. See the License for the
-// specific language governing permissions and limitations under the License.
+//Copyright (c) 2015, United States Government, as represented by the
+//Administrator of the National Aeronautics and Space Administration.
+//All rights reserved.
+
+//The xGDS platform is licensed under the Apache License, Version 2.0
+//(the "License"); you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//http://www.apache.org/licenses/LICENSE-2.0.
+
+//Unless required by applicable law or agreed to in writing, software distributed
+//under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+//CONDITIONS OF ANY KIND, either express or implied. See the License for the
+//specific language governing permissions and limitations under the License.
 //__END_LICENSE__
 
 var xgds_sample = xgds_sample || {};
+
 $.extend(xgds_sample,{
+	showReplicateOptions: function() {
+		// Replicates Legend:
+		// Biology = A, B, C
+		// ORG = D, E
+		// Geology 
+		// Archive
+		var selected = $("#id_sample_type option:selected").html();
+		$("#id_replicate").children('option').hide();
+		$("#id_replicate").removeAttr('disabled');
+		if (selected == "Biology") {
+			$("#id_replicate option[value='1']").show();
+			$("#id_replicate option[value='2']").show();
+			$("#id_replicate option[value='3']").show();
+		} else if (selected == "ORG") {
+			$("#id_replicate option[value='4']").show();
+			$("#id_replicate option[value='5']").show();
+		} else if ((selected == "Archive") || (selected == "Geology")) {
+			$("#id_replicate").val('');
+			$("#id_replicate").attr('disabled','disabled')
+		} else if (selected = "Special") {
+			$("#id_replicate option[value='6']").show();
+			$("#id_replicate option[value='7']").show();
+			$("#id_replicate option[value='8']").show();
+			$("#id_replicate option[value='9']").show();
+			$("#id_replicate option[value='10']").show();
+		}
+	},
+	
 	initializeSampleEditForm: function(){
 		$('#id_resource').parent().parent().hide();
 		$('#id_latitude').parent().parent().hide();
 		$('#id_longitude').parent().parent().hide();
 		$('#id_altitude').parent().parent().hide();
 		$('#id_flight').parent().parent().hide();
+		$( ".target" ).change(function() { alert( "Handler for .change() called." );});
 		this.setupCollectorInput();
+		this.showReplicateOptions();
+		// hook up the show replicate options to type change.
+		var _this = this;
+		$('#id_sample_type').change(_this.showReplicateOptions);
+		
 	},
+	
 	toggleAdvancedInput: function() {
 		$('#id_resource').parent().parent().toggle();
 		$('#id_latitude').parent().parent().toggle();
@@ -36,41 +72,37 @@ $.extend(xgds_sample,{
 			$('.toggleInputFields').html('Open out-of-sim fields');	
 		}
 	},
+	
 	setupCollectorInput: function() {
 		// typeahead autocomplete for input fields
-
 		var substringMatcher = function(strs) {
-			  return function findMatches(q, cb) {
-			    var matches, substringRegex;
+			return function findMatches(q, cb) {
+				var matches, substringRegex;
+				// an array that will be populated with substring matches
+				matches = [];
+				// regex used to determine if a string contains the substring `q`
+				substrRegex = new RegExp(q, 'i');
+				// iterate through the pool of strings and for any string that
+				// contains the substring `q`, add it to the `matches` array
+				$.each(strs, function(i, str) {
+					if (substrRegex.test(str)) {
+						matches.push(str);
+					}
+				});
 
-			    // an array that will be populated with substring matches
-			    matches = [];
-
-			    // regex used to determine if a string contains the substring `q`
-			    substrRegex = new RegExp(q, 'i');
-
-			    // iterate through the pool of strings and for any string that
-			    // contains the substring `q`, add it to the `matches` array
-			    $.each(strs, function(i, str) {
-			      if (substrRegex.test(str)) {
-			        matches.push(str);
-			      }
-			    });
-
-			    cb(matches);
-			  };
+				cb(matches);
 			};
+		};
 
-			
 		$('#id_collector').typeahead({
-			  hint: true,
-			  highlight: true,
-			  minLength: 1
-			},
-			{
-			  name: 'collector',
-			  source: substringMatcher(collectors)
-			});
+			hint: true,
+			highlight: true,
+			minLength: 1
+		},
+		{
+			name: 'collector',
+			source: substringMatcher(collectors)
+		});
 	}
 
 });
